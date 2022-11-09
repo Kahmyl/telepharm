15dec5f5407335b62e63dd9a6f0d67f9b1067c56
kahmyl <kamilhabib556@gmail.com>
2022-11-09 23:08:20 +0100

    updated UIs and connected apis

--- src/components/Doctor/Appointment/index.tsx
+++ src/components/Doctor/Appointment/index.tsx
@@ -1,10 +1,92 @@
-import { useState } from "react";
-import { TouchableOpacity, View } from "react-native";
-import AppointmentTabs from "../../Appointment";
+import { Button } from "@rneui/themed";
+import { useCallback, useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useAppDispatch, useAppSelector } from "../../../app/hooks";
+import { getAppointments } from "../../../store/actions/appointment";
+import Card from "../../Card";
 import { AppointmentContainer } from "../../Patient/Appointment/styles";
+import { Seperator } from "../../Patient/Specialist/styles";
 import { TabContainer, TabPartition, TabText } from "../../User/styles";
+import { Centered } from "../Home/styles";
+import { BaseText } from "./styles";
 const DoctorAppointmentContent = () => {
   const [tab, setTab] = useState("active");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const [error, setError] = useState("");
+  const { appointments, total } = useAppSelector((store) => store.appointments);
+  const dispatch = useAppDispatch();
+
+  const handleAppointments = useCallback(async () => {
+    setError("");
+    setIsRefreshing(true);
+    try {
+      await dispatch(getAppointments(tab));
+    } catch (error) {
+      console.log(error);
+      setError("Something went wrong");
+    }
+    setIsRefreshing(false);
+  }, [dispatch, setIsLoading, setError]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    handleAppointments().then(() => {
+      setIsLoading(false);
+    });
+  }, [dispatch, handleAppointments, tab]);
+
+  function renderElement() {
+    if (error) {
+      return (
+        <Centered>
+          <BaseText>An error occured.</BaseText>
+          <Button
+            color="secondary"
+            title="Try again"
+            onPress={handleAppointments}
+          />
+        </Centered>
+      );
+    } else if (isLoading) {
+      return (
+        <Centered>
+          <ActivityIndicator size="large" color="grey" />
+        </Centered>
+      );
+    } else if (
+      !isLoading &&
+      !error &&
+      appointments &&
+      appointments.length === 0
+    ) {
+      return (
+        <Centered>
+          <BaseText>No upcoming appointment</BaseText>
+        </Centered>
+      );
+    } else {
+      return (
+        <FlatList
+          onRefresh={handleAppointments}
+          refreshing={isRefreshing}
+          data={appointments}
+          keyExtractor={(item: any) => item._id}
+          ItemSeparatorComponent={() => {
+            return <Seperator />;
+          }}
+          renderItem={({ item }: any) => {
+            return <Card data={item} />;
+          }}
+        />
+      );
+    }
+  }
   return (
     <AppointmentContainer>
       <TabContainer>
@@ -16,13 +98,7 @@ const DoctorAppointmentContent = () => {
           <TabText active={tab === "pending"}>Pending</TabText>
         </TouchableOpacity>
       </TabContainer>
-      <View style={{ backgroundColor: " #2192ff", height: "100%" }}>
-        {tab === "active" ? (
-          <AppointmentTabs status={tab} />
-        ) : (
-          <AppointmentTabs status={tab} />
-        )}
-      </View>
+      <View style={{ position: "relative", height: "100%" }}>{renderElement()}</View>
     </AppointmentContainer>
   );
 };
---
+++ src/components/Doctor/Appointment/styles.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components/native";
+
+export const BaseText = styled.Text`
+  font-family: "Inter_500Medium";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 29px;
+  text-align: center;
+  color: #ffffff;
+`;
